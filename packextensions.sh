295e32a47d0344adf619927c2836108f18b3cca6
Jonas Gillberg <jonte.gillberg@gmail.com>
2020-01-18 22:25:07 +0100

    * fixed buildall.sh

--- packextensions.sh
+++ packextensions.sh
@@ -1,5 +1,14 @@
-cp -v manifest_chrome.json MagicCardSpotter/manifest.json
-VERSION='cat VERSION.txt'
-sed -iv 's/VERSION/${VERSION}/g' MagicCardSpotter/manifest.json
+cp -f manifest_chrome.json MagicCardSpotter/manifest.json
+VERSION=`cat VERSION.txt`
+sed -i "s/VERSION/${VERSION}/g" MagicCardSpotter/manifest.json
+echo Packing Chrome ${VERSION}
+rm -fv "MagicCardSpotterChrome${VERSION}.zip"
+cd MagicCardSpotter
+zip -r "../MagicCardSpotterChrome${VERSION}.zip" *
+cd ..
 
-zip MagicCardSpotter/* MagicCardSpotter${VERSION}.zip
+cp -f manifest_ff.json MagicCardSpotter/manifest.json
+sed -i "s/VERSION/${VERSION}/g" MagicCardSpotter/manifest.json
+echo Packing Firefox ${VERSION}
+rm -fv "MagicCardSpotterFirefox${VERSION}.zip"
+zip -r "MagicCardSpotterFirefox${VERSION}.zip" MagicCardSpotter
